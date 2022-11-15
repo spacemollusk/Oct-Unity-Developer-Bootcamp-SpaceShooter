1dd00031f6ff19ba666a98f7c9b42ec651b34b14
cyber6 <nakisa.donnelly@gmail.com>
2022-11-15 15:53:38 -0800

    WASD and arrow keys for movement

--- Assets/Space Shooter Template FREE/Scripts/PlayerMoving.cs
+++ Assets/Space Shooter Template FREE/Scripts/PlayerMoving.cs
@@ -40,13 +40,15 @@ public class PlayerMoving : MonoBehaviour {
         if (controlIsActive)
         {
 #if UNITY_STANDALONE || UNITY_EDITOR    //if the current platform is not mobile, setting mouse handling 
+            var movementDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+            transform.position += movementDirection.normalized * 15 * Time.deltaTime;
 
-            if (Input.GetMouseButton(0)) //if mouse button was pressed       
-            {
-                Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition); //calculating mouse position in the worldspace
-                mousePosition.z = transform.position.z;
-                transform.position = Vector3.MoveTowards(transform.position, mousePosition, 30 * Time.deltaTime);
-            }
+            //if (Input.GetMouseButton(0)) //if mouse button was pressed       
+            //{
+            //    Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition); //calculating mouse position in the worldspace
+            //    mousePosition.z = transform.position.z;
+            //    transform.position = Vector3.MoveTowards(transform.position, mousePosition, 30 * Time.deltaTime);
+            //}
 #endif
 
 #if UNITY_IOS || UNITY_ANDROID //if current platform is mobile, 
